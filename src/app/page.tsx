ae3597cd949f36a9a94b0382b6802ad37d291ab0
junjuny0227 <junjuny0227@gmail.com>
2025-02-16 23:34:20 +0900

    client side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,35 @@
+"use client";
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Main = () => {
-  return <h1>next first</h1>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const getMovie = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://nomad-movies.nomadcoders.workers.dev/movies"
+        );
+        setMovies(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getMovie();
+  }, []);
+
+  return (
+    <div>
+      <h1>next first</h1>
+      {isLoading ? "Loading..." : JSON.stringify(movies)}
+    </div>
+  );
 };
 
 export default Main;

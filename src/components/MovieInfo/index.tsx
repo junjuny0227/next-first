6766c52c7bfa8b8cc16f4d40c546706005ce0620
junjuny0227 <junjuny0227@gmail.com>
2025-02-17 21:45:40 +0900

    movieInfo

---
+++ src/components/MovieInfo/index.tsx
@@ -0,0 +1,19 @@
+import { BASE_URL } from "@/app/page";
+import axios from "axios";
+
+interface MovieInfoProps {
+  id: string;
+}
+
+const getMovie = async (id: string) => {
+  const { data } = await axios.get(`${BASE_URL}/movies/${id}`);
+  return data;
+};
+
+const MovieInfo = async ({ id }: MovieInfoProps) => {
+  const movie = await getMovie(id);
+
+  return <h6>{JSON.stringify(movie)}</h6>;
+};
+
+export default MovieInfo;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Header } from "./Header";
 export { default as MovieVideos } from "./MovieVideos";
+export { default as MovieInfo } from "./MovieInfo";

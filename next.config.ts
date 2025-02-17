0da24c0a7abd967547af43678a05bba145fe7c78
junjuny0227 <junjuny0227@gmail.com>
2025-02-17 22:06:55 +0900

    caching

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    staleTimes: {
+      dynamic: 60 * 10,
+    },
+  },
 };
 
 export default nextConfig;

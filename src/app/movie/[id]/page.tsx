fd353ccd98e8944464caee7ba1f2edff59ee2e60
junjuny0227 <junjuny0227@gmail.com>
2025-02-17 20:52:37 +0900

    영화 정보 가져오기

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,13 +1,27 @@
+import { BASE_URL } from "@/app/page";
+import axios from "axios";
+
 interface MovieDetailProps {
   params: {
     id: string;
   };
 }
 
+const getMovie = async (id: string) => {
+  const { data } = await axios.get(`${BASE_URL}/movies/${id}`);
+  return data;
+};
+
+const getVideos = async (id: string) => {
+  const { data } = await axios.get(`${BASE_URL}/movies/${id}/videos`);
+  return data;
+};
+
 const MovieDetail = async ({ params }: MovieDetailProps) => {
   const { id } = await params;
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
 
-  return <h1>Movie Detail: {id}</h1>;
+  return <h1>{movie.title}</h1>;
 };
 
 export default MovieDetail;

63b000ac2d0d2ca733af8f59a1655c1da0031b17
junjuny0227 <junjuny0227@gmail.com>
2025-02-20 14:56:24 +0900

    useRouter

--- src/components/Movie/index.tsx
+++ src/components/Movie/index.tsx
@@ -2,6 +2,8 @@
 
 import * as S from "./style";
 
+import { useRouter } from "next/navigation";
+
 interface MovieProps {
   id: number;
   poster_path: string;
@@ -9,9 +11,17 @@ interface MovieProps {
 }
 
 const Movie = ({ id, poster_path, title }: MovieProps) => {
+  const router = useRouter();
+
   return (
     <S.MovieContainer>
-      <S.StyledImage src={poster_path} alt={title} width={400} height={500} />
+      <S.StyledImage
+        src={poster_path}
+        alt={title}
+        width={400}
+        height={500}
+        onClick={() => router.push(`/movie/${id}`)}
+      />
       <S.StyledLink href={`/movie/${id}`}>{title}</S.StyledLink>
     </S.MovieContainer>
   );
